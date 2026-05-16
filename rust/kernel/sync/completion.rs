5d7319d600b987f7601fc0dd0ff826656645bb7f
Manos Pitsidianakis <manos@pitsidianak.is>
2026-05-17 00:20:23 +0300

    rust/kernel/completion: add complete()
    Add wrapper method for `complete()`
    
    (we currently have only `complete_all()`).
    
    Signed-off-by: Manos Pitsidianakis <manos@pitsidianak.is>

--- rust/kernel/sync/completion.rs
+++ rust/kernel/sync/completion.rs
@@ -97,6 +97,15 @@ impl Completion {
         unsafe { bindings::reinit_completion(self.as_raw()) };
     }
 
+    /// Signal a single task waiting on this completion.
+    ///
+    /// This method wakes up a single task waiting on this completion.
+    #[inline]
+    pub fn complete(&self) {
+        // SAFETY: `self.as_raw()` is a pointer to a valid `struct completion`.
+        unsafe { bindings::complete(self.as_raw()) };
+    }
+
     /// Signal all tasks waiting on this completion.
     ///
     /// This method wakes up all tasks waiting on this completion; after this operation the
